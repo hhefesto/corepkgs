0fe9da3495a4d6f82ebf4e5b7401800c4ce45e18
Aliaksandr <grubian2@gmail.com>
2026-09-03 18:11:25 +0300

    config: cleanup always true options

--- config/build-options.nix
+++ config/build-options.nix
@@ -73,18 +73,6 @@ in
       feature = "run `checkPhase` by default";
     };
 
-    strictDepsByDefault = mkMassRebuild {
-      feature = "set `strictDeps` to true by default";
-    };
-
-    structuredAttrsByDefault = mkMassRebuild {
-      feature = "set `__structuredAttrs` to true by default";
-    };
-
-    enableParallelBuildingByDefault = mkMassRebuild {
-      feature = "set `enableParallelBuilding` to true by default";
-    };
-
     configurePlatformsByDefault = mkMassRebuild {
       feature = "set `configurePlatforms` to `[\"build\" \"host\"]` by default";
     };
