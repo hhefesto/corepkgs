6c3c9fad0d651054ff468f89c8451b25d720bec8
Aliaksandr <grubian2@gmail.com>
2026-09-07 00:22:27 +0300

    vmTools: support structuredAttrs

--- build-support/vm/default.nix
+++ build-support/vm/default.nix
@@ -569,7 +569,7 @@ let
 
           echo "unpacking RPMs..."
           set +o pipefail
-          for i in $rpms; do
+          for i in "''${rpms[@]}"; do
               echo "$i..."
               ${rpm}/bin/rpm2cpio "$i" | chroot /mnt ${cpio}/bin/cpio -i --make-directories --unconditional
           done
@@ -586,7 +586,7 @@ let
 
           echo "installing RPMs..."
           PATH=/usr/bin:/bin:/usr/sbin:/sbin $chroot /mnt \
-            rpm -iv --nosignature ${lib.optionalString (!runScripts) "--noscripts"} $rpms
+            rpm -iv --nosignature ${lib.optionalString (!runScripts) "--noscripts"} "''${rpms[@]}"
 
           echo "running post-install script..."
           eval "$postInstall"
@@ -646,10 +646,11 @@ let
           dontConfigure = true;
 
           outDir = "rpms/${attrs.diskImage.name}";
+          extraRPMs = attrs.extraRPMs or [ ];
 
           prepareImagePhase = ''
-            if test -n "$extraRPMs"; then
-              for rpmdir in $extraRPMs ; do
+            if ((''${#extraRPMs[@]})); then
+              for rpmdir in "''${extraRPMs[@]}"; do
                 rpm -iv $(ls $rpmdir/rpms/*/*.rpm | grep -v 'src\.rpm' | sort | head -1)
               done
             fi
@@ -731,7 +732,7 @@ let
             memSize
             ;
 
-          debs = (lib.intersperse "|" debs);
+          debs = map toString debs;
 
           preVM = createEmptyImage { inherit size fullName; };
 
@@ -750,11 +751,11 @@ let
             # (which have lots of circular dependencies) from barfing.
             echo "unpacking Debs..."
 
-            for deb in $debs; do
-              if test "$deb" != "|"; then
+            for component in "''${debs[@]}"; do
+              for deb in $component; do
                 echo "$deb..."
                 dpkg-deb --extract "$deb" /mnt
-              fi
+              done
             done
 
             # Make the Nix store available in /mnt, because that's where the .debs live.
@@ -777,10 +778,7 @@ let
 
             export DEBIAN_FRONTEND=noninteractive
 
-            oldIFS="$IFS"
-            IFS="|"
-            for component in $debs; do
-              IFS="$oldIFS"
+            for component in "''${debs[@]}"; do
               echo
               echo ">>> INSTALLING COMPONENT: $component"
               debs=
