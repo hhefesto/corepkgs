5f4d0c3d8545a7fe407bf129b54a2ab2c4d6edd8
Aliaksandr <grubian2@gmail.com>
2026-09-07 00:22:12 +0300

    dpdk-kmods: expand makeFlags as an array

--- pkgs/linux-support/pkgs/dpdk-kmods/default.nix
+++ pkgs/linux-support/pkgs/dpdk-kmods/default.nix
@@ -27,7 +27,7 @@ stdenv.mkDerivation rec {
   preBuild = "cd linux/igb_uio";
 
   installPhase = ''
-    make -C ${KSRC} M=$(pwd) modules_install $makeFlags
+    make -C ${KSRC} M=$(pwd) modules_install "''${makeFlags[@]}"
   '';
 
   enableParallelBuilding = true;
