6f4b20f0e678536f3b5e05ef683c77ec00506ad7
Aliaksandr <grubian2@gmail.com>
2026-09-07 00:22:26 +0300

    makeDBusConf: support structuredAttrs

--- pkgs/dbus/make-dbus-conf.nix
+++ pkgs/dbus/make-dbus-conf.nix
@@ -33,13 +33,13 @@ runCommand "dbus-1"
     mkdir -p $out
 
     xsltproc --nonet \
-      --stringparam serviceDirectories "$serviceDirectories" \
+      --stringparam serviceDirectories "''${serviceDirectories[*]}" \
       --stringparam suidHelper "$suidHelper" \
       --stringparam apparmor "$apparmor" \
       ${./make-system-conf.xsl} ${dbus}/share/dbus-1/system.conf \
       > $out/system.conf
     xsltproc --nonet \
-      --stringparam serviceDirectories "$serviceDirectories" \
+      --stringparam serviceDirectories "''${serviceDirectories[*]}" \
       --stringparam apparmor "$apparmor" \
       ${./make-session-conf.xsl} ${dbus}/share/dbus-1/session.conf \
       > $out/session.conf
